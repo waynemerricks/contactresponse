ec191621aae5afb0c589615c2d69d0c58b2a7837
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-15 22:54:25 +0100

    Added functions required to login

---
+++ www/includes/login.php
@@ -0,0 +1,59 @@
+<?php
+
+  /**
+   * Checks if user can login and updates failed counts as appropriate
+   * If we can login, user is redirected to main.php
+   * @param $mysqli MySQLi connection
+   * @param $username User Name of person to check
+   * @param $password clear text password they typed in
+   */
+  function checkLogin($mysqli, $username, $password){
+
+    $mysqli = getDatabaseRead();//Get read connection
+
+    //Verify User Password
+    $sql = 'SELECT `password`, `id` FROM `users` WHERE `login_name` = ?';
+
+    if($stmt = $mysqli->prepare($sql) && $stmt->bind_param('s',
+         $username) && $stmt->execute()){
+
+      $stmt->bind_result($hash, $id);
+
+      while($stmt->fetch()){
+
+        if(password_verify($password, $hash)){
+
+          $_SESSION['userid'] = $id; //Set logged in user ID
+          header('Location: main.php');
+
+        }else{
+
+          incrementLoginFail();
+          $loginError = TRUE;
+
+        }
+
+      }
+
+    }else{
+
+      die('Failed to get login info');
+      incrementLoginFail();
+
+    }
+
+  }
+
+  /**
+   * Increments the SESSION['failedLogins'] variable
+   */
+  function incrementLoginFail(){
+
+    if(isset($_SESSION['failedLogins']))
+      $_SESSION['failedLogins']++;
+    else
+      $_SESSION['failedLogins'] = 1;
+
+  }
+
+?>
