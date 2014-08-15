5e4ca35c50da2cf3a51295eb806026c3cc481640
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-15 22:52:46 +0100

    Added getRead/Write connections to database.php

--- www/includes/database.php
+++ www/includes/database.php
@@ -1,20 +1,37 @@
 <?php
 
-  /**
-   * Returns an array with database credentials
-   * Change as appropriate
-   */
-  function getDatabase(){
+  include('credentials.php');//DB User/Pass details
 
-    $DB = array();
-    $DB['readHostname'] = 'localhost';
-    $DB['readUsername'] = 'changeToUserName';
-    $DB['readPassword'] = 'changeToPassword';
-    $DB['readDatabase'] = 'changeToDatabase';
-    $DB['writeHostname'] = 'localhost';
-    $DB['writeUsername'] = 'changeToUserName';
-    $DB['writePassword'] = 'changeToPassword';
-    $DB['writeDatabase'] = 'changeToDatabase';
+  /**
+   * Returns a MySQLi Read connection or dies with an error
+   */
+  function getDatabaseRead(){
+
+    $DB = getCredentials('read');
+
+    $mysqli = new $mysqli($DB['hostname'], $DB['username'], $DB['password'],
+                       $DB['database']);
+
+    if($mysqli->connect_errno)
+      die('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error;
+
+    return $mysqli;
+
+  }
+
+  /**
+   * Returns a MySQLi Write connection or dies with an error
+   */
+  function getDatabaseWrite(){
+
+    $DB = getCredentials('write');
+
+    if($mysqli->connect_errno)
+      die('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error;
+
+    return $mysqli;
 
   }
 
--- www/index.php
+++ www/index.php
@@ -1,14 +1,40 @@
 <?php
 
-  include('includes/database.php');
-  include('includes/sessions.php');
+  include('includes/sessions.php'); //Include SESSION stuff
+  include('includes/database.php'); //Include DB stuff
+  include('includes/login.php'); //Include login functions
+
+  $loginError = FALSE;//Flag for failed to login or not
+
+  if(isset($_POST['login'], $_POST['password'])){
+
+    $mysqli = getDatabaseRead();//Get read connection
+
+    //Verify User Password
+    checkLogin($mysqli, $_POST['login'], $_POST['password']);
+
+  }
 
 ?>
 <html>
   <head>
-    <title>CRS</title>
+    <title>CRS | Login</title>
   </head>
   <body>
-    Place Holder HTML
+    <div id="error">
+      <?php if($loginError){ ?>
+        Invalid User name or password please try again
+      <?php } ?>
+    </div>
+
+    <?php if($_SESSION['failedLogins'] < 3){ ?>
+      <div id="login">
+        <form method="post">
+          <input id="login" name="login" type="text" value=""></input>
+          <input id="password" name="password" type="password" value=""></input>
+          <input id="submit" type="submit" value="login"></input>
+        </form>
+      </div>
+    <?php } ?>
   </body>
 </html>
