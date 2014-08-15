541b88c64b085c73d79bf757f49d46f87a49ac57
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-15 21:08:06 +0100

    Added database.php to store DB username / password config Added session.php place holder for session_start and any other pre load work

---
+++ www/includes/database.php
@@ -0,0 +1,21 @@
+<?php
+
+  /**
+   * Returns an array with database credentials
+   * Change as appropriate
+   */
+  function getDatabase(){
+
+    $DB = array();
+    $DB['readHostname'] = 'localhost';
+    $DB['readUsername'] = 'changeToUserName';
+    $DB['readPassword'] = 'changeToPassword';
+    $DB['readDatabase'] = 'changeToDatabase';
+    $DB['writeHostname'] = 'localhost';
+    $DB['writeUsername'] = 'changeToUserName';
+    $DB['writePassword'] = 'changeToPassword';
+    $DB['writeDatabase'] = 'changeToDatabase';
+
+  }
+
+?>
