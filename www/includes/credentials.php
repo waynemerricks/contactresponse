5af82003ec2b343aa303376f7d1c546e85ce8b59
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-16 11:30:04 +0100

    Dumbassery caused by late night coding, all vars or write in credentials should be just hostname, username, password and database.  write/read is not necessary after refactoring but brain did not realise this!

--- www/includes/credentials.php
+++ www/includes/credentials.php
@@ -7,18 +7,18 @@
   function getCredentials($readWrite){
 
     $DB_READ = array();
-    $DB_READ['readHostname'] = 'localhost';
-    $DB_READ['readUsername'] = 'changeToUserName';
-    $DB_READ['readPassword'] = 'changeToPassword';
-    $DB_READ['readDatabase'] = 'changeToDatabase';
+    $DB_READ['hostname'] = 'localhost';
+    $DB_READ['username'] = 'changeToUserName';
+    $DB_READ['password'] = 'changeToPassword';
+    $DB_READ['database'] = 'changeToDatabase';
 
     //If you have separate WRITE credentials enter them here
     //Otherwise just remark out this section
     $DB_WRITE = array();
-    $DB_WRITE['writeHostname'] = 'localhost';
-    $DB_WRITE['writeUsername'] = 'changeToUserName';
-    $DB_WRITE['writePassword'] = 'changeToPassword';
-    $DB_WRITE['writeDatabase'] = 'changeToDatabase';
+    $DB_WRITE['hostname'] = 'localhost';
+    $DB_WRITE['username'] = 'changeToUserName';
+    $DB_WRITE['password'] = 'changeToPassword';
+    $DB_WRITE['database'] = 'changeToDatabase';
 
     $DB = NULL;
 
