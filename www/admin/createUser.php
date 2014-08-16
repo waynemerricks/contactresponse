cb044a4d7d1c57cbaaba64612aa7ecee47aaea35
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-16 12:01:05 +0100

    Implemented createUser TODO remove SESSION debug
    Will check for username & password compliance before creating an account

--- www/admin/createUser.php
+++ www/admin/createUser.php
@@ -1,11 +1,12 @@
 <?php
 
   include('../includes/session.php');
+  include('includes/createUser.php');
 
-  //DEBUG 
+  //DEBUG
   $_SESSION['isAdmin'] = TRUE;
   $_SESSION['canCreateUsers'] = TRUE;
-  
+
   if(!isset($_SESSION['isAdmin']))
     header('Location: ../index.php');//Not an admin so go away
   else if(!isset($_SESSION['canCreateUsers']))//Admin but can't create users
@@ -14,18 +15,37 @@
   include('../includes/database.php');
 
   $createdUser = FALSE;
+  $invalidLoginPassword = FALSE;
 
-  if(isset($_SESSION['canCreateUsers'], $_POST['userName'], $_POST['email'],
+  if(isset($_SESSION['canCreateUsers'], $_POST['login'],
        $_POST['password']) && $_SESSION['canCreateUsers']){
 
-    $mysqli = getDatabaseWrite();
+    if(isAcceptableLogin($_POST['login']) && isAcceptablePassword(
+         $_POST['password'])){
 
-    $sql = 'INSERT INTO `users` (`login_name`, `password`) VALUES (?, ?);
+      $mysqli = getDatabaseWrite();
 
-    if($stmt = $mysqli->prepare($sql) && $stmt->bind_param('ss',
-         $_POST['username'], password_hash($_POST['password'],
-         PASSWORD_DEFAULT)) && $stmt->execute())
-      $createdUser = TRUE;
+      $sql = 'INSERT INTO `users` (`login_name`, `password`) VALUES (?, ?)';
+
+      $stmt = NULL;
+
+      if($stmt = $mysqli->prepare($sql)){
+        if($stmt->bind_param('ss',$_POST['login'], 
+             password_hash($_POST['password'],PASSWORD_DEFAULT))){
+
+          if(!$stmt->execute())
+            die('Execute Error: ' . $stmt->error);
+          else
+            $createdUser = TRUE;
+
+        }else
+          die('Bind Error: ' . $stmt->error);
+
+      }else
+        die('Prepare Error: ' . $mysqli->error);
+
+    }else
+      $invalidLoginPassword = TRUE;
 
   }
 
@@ -37,15 +57,23 @@
   </head>
   <body>
     <div id="created">
-      <?php if($createdUser){ ?>
+      <?php if($createdUser === TRUE){ ?>
         User Created Succesfully
+      <?php } ?>
+      <?php if($invalidLoginPassword === TRUE){ ?>
+        You have chosen an unsuitable user name or password.<br><br>
+        User Names should be at least 8 characters long<br><br>
+        Passwords should also be at least 8 characters long. They must contain 
+        at least 1 number and 1 upper case letter.<br>
       <?php } ?>
     </div>
     <div id="login">
-      <form method="post">
-        <label for="login">User Name:</label><input id="login" name="login" type="text" value=""></input><br>
-        <label for="password">Password:</label><input id="password" name="password" type="password" value=""></input><br>
-        <input id="submit" type="submit" value="login"></input><br>
+      <form method="post" action="createUser.php">
+        <label for="login">User Name:</label>
+        <input id="userName" name="login" type="text" value="" /><br>
+        <label for="password">Password:</label>
+        <input id="password" name="password" type="password" value="" /><br>
+        <input id="submit" type="submit" value="create" /><br>
       </form>
     </div>
   </body>
