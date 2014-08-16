1900448401e6524e79dad93fc50bcaee5f72c909
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-16 12:00:09 +0100

    Finctions for admin createuser.php

---
+++ www/admin/includes/createUser.php
@@ -0,0 +1,34 @@
+<?php
+
+  /**
+   * Checks to see if the login name is at least 8 characters
+   */
+  function isAcceptableLogin($name){
+
+    $acceptable = FALSE;
+
+    if(strlen($name) > 7)
+      $acceptable = TRUE;
+
+    return $acceptable;
+
+  }
+
+  /**
+   * Checks to see if the password is at least 8 characters
+   * and contains at least one number and one upper/lower case
+   * character
+   */
+  function isAcceptablePassword($password){
+
+    $acceptable = FALSE;
+
+    if(strlen($password) > 7 && preg_match('/[A-Za-z]/', $password) &&
+          preg_match('/[0-9]/', $password))
+      $acceptable = TRUE;
+
+    return $acceptable;
+
+  }
+
+?>
