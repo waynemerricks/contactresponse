3b13721fc747b80cb45d2a57f7d6c61877a02130
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-17 17:17:37 +0100

    Added logoutSession function to unset and destroy $_SESSION when logging out

--- www/includes/session.php
+++ www/includes/session.php
@@ -2,4 +2,25 @@
 
   session_start();
 
+  /**
+   * Destroys the session and unsets $_SESSION vars
+   * Then redirects to root/index.php
+   */
+  function logoutSession(){
+
+    if(isset($_SESSION['user'])
+      unset($_SESSION['user']);
+
+    if(isset($_SESSION['userid'])
+      unset($_SESSION['userid']);
+
+    if(isset($_SESSION['failedLogins'])
+      unset($_SESSION['failedLogins']);
+
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+
+  }
+
 ?>
