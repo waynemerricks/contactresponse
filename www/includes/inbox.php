605b2f85bdaaf5da3b0a1c4ad276f0842d596902
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-18 00:51:28 +0100

    Moved isDefaultHelper to user.php functions

--- www/includes/inbox.php
+++ www/includes/inbox.php
@@ -56,20 +56,4 @@
 
   }
 
-  /**
-   * Returns true if this user is the default helper (e.g. where contacts
-   * go if not assigned elsewhere)
-   */
-  function isDefaultHelper(){
-
-    $default = FALSE;
-
-    if(isset($_SESSION['user']['permissions']) && in_array('DEFAULT_HELPER',
-          $_SESSION['user']['permissions']))
-      $default = TRUE;
-
-    return $default;
-
-  }
-
 ?>
