df188089179ec68537fb4292e6a5a8bfe766fc42
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-17 15:24:12 +0100

    Placeholder logged in -> main.php

---
+++ www/main.php
@@ -0,0 +1,15 @@
+<?php
+
+  include('includes/session.php'); //Include SESSION stuff
+  include('includes/database.php'); //Include DB stuff
+
+?>
+<html>
+  <head>
+    <title>CRS | Login</title>
+    <link rel="stylesheet" type="text/css" href="crs.css">
+  </head>
+  <body>
+    Place Holder Logged in successfully
+  </body>
+</html>
