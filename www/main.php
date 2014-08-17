78567c0800220442256cd7edd276c5d3f7b5463d
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-17 17:18:21 +0100

    BUG FIX: Needed $mysqli read connection before loggedIn include

--- www/main.php
+++ www/main.php
@@ -1,8 +1,12 @@
 <?php
 
   include('includes/session.php'); //Include SESSION stuff
-  include('includes/loggedIn.php'); //Include logged in check
   include('includes/database.php'); //Include DB stuff
+
+  $mysqli = getDatabaseRead(); //Get MySQL connection for page
+
+  include('includes/loggedIn.php'); //Include logged in check
+
 
 ?>
 <html>
@@ -11,6 +15,12 @@
     <link rel="stylesheet" type="text/css" href="crs.css">
   </head>
   <body>
-    Place Holder Logged in successfully: User ID <?php echo $_SESSION['userid']; ?>
+    Place Holder Logged in successfully: User ID <?php echo $_SESSION['userid']; ?><br>
+    <?php print_r($_SESSION); ?>
   </body>
 </html>
+<?php
+
+  $mysqli->close();//Close MySQL connection
+
+?>
