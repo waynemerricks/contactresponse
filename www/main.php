9193ea58978ffb9a5c5291ded2e7da5bb90635c0
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-17 17:43:32 +0100

    Implemented Skeleton of main user view

--- www/main.php
+++ www/main.php
@@ -6,7 +6,9 @@
   $mysqli = getDatabaseRead(); //Get MySQL connection for page
 
   include('includes/loggedIn.php'); //Include logged in check
+  include('includes/inbox.php'); //Functions for getting the inbox of this user
 
+  $contactsPending = getInbox(getUserId());
 
 ?>
 <html>
@@ -15,8 +17,25 @@
     <link rel="stylesheet" type="text/css" href="crs.css">
   </head>
   <body>
-    Place Holder Logged in successfully: User ID <?php echo $_SESSION['userid']; ?><br>
-    <?php print_r($_SESSION); ?>
+    <div class="inbox">
+      <table class="inbox">
+        <tr>
+          <th>Contact</th>
+          <th>Messages</th>
+          <th>Date</th>
+        </tr>
+        <?php for($i = 0; $i < sizeof($contactsPending); $i++){ ?>
+          <tr>
+            <td class="name"><?php echo $contactsPending[$i]['name']; ?></td>
+            <td><?php echo $contactsPending[$i]['numberOfMessages']; ?></td>
+            <td class="time"><?php echo $contactsPending[$i]['date']; ?></td>
+          </tr>
+          <tr class="preview">
+            <td class="preview"><?php echo $contactsPending[$i]['preview']; ?></td>
+          </tr>
+        <?php } ?>
+      </table>
+    </div>
   </body>
 </html>
 <?php
