2c439953c4f1c5051743eb60a00baf93beb80607
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-24 23:24:47 +0100

    Added getLoggedInUserID & getDelegatesList
    LoggedInUserID returns the id of the user who is logged in
    Delegates returns a list of names and ids of users we can delegate to

--- www/includes/user.php
+++ www/includes/user.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+  /**
    * Returns true if this user is the default helper (e.g. where contacts
    * go if not assigned elsewhere)
    */
@@ -13,6 +13,43 @@
       $default = TRUE;
 
     return $default;
+
+  }
+
+  /**
+   * Returns the id of the logged in user from $_SESSION['user']
+   */
+  function getLoggedInUserID(){
+
+    $id = NULL;
+    if(isset($_SESSION['user']))
+      $id = $_SESSION['user']['id'];
+
+    return $id;
+
+  }
+
+  /**
+   * Returns a list of users we can delegate to
+   */
+  function getDelegatesList($mysqli){
+
+    $delegates = array();
+
+    //TODO Restrict based on user thats logged in
+    $sql = 'SELECT `id`, `name` FROM `users` ORDER BY `name` ASC';
+    $result = $mysqli->query($sql);
+
+    while($row = $result->fetch_assoc()){
+
+      $delegate = array();
+      $delegate['id'] = $row['id'];
+      $delegate['name'] = $row['name'];
+      $delegates[] = $delegate;
+
+    }
+
+    return $delegates;
 
   }
 
