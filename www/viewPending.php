6f120b5d09f79cc66d8f8644fbfe930590039a04
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-24 23:25:41 +0100

    Auto Replies and Delegates update from DB.  Added skeleton JS functions

--- www/viewPending.php
+++ www/viewPending.php
@@ -9,6 +9,7 @@
   include('includes/messages.php'); //Functions for getting messages
   include('includes/contact.php'); //Functions for getting a contact
   include('includes/user.php'); //Functions relating to logged in user
+  include('includes/autoreplies.php'); //Functions relating to auto replies
 
   $pendingMessages = array();
 
@@ -16,7 +17,12 @@
 
     $pendingMessages = getPendingMessages(getUserId(), $_GET['id'], 
         isDefaultHelper(), $mysqli);
+    $lastMessageID = -1;//Set this to the id of newest message (last in array)
+
     $contact = new Contact($_GET['id'], $mysqli);
+
+    $autoReply = getAutoReplyLabels($mysqli);
+    $delegates = getDelegatesList($mysqli);
 
   }else
     header('Location: main.php');//We got here weirdly so just go back to main
@@ -29,6 +35,42 @@
     <link rel="stylesheet" type="text/css" href="css/crs.css">
     <link rel="stylesheet" type="text/css" href="css/crs_viewPending.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <script type="text/javascript">
+      /** JS Sets this contact as auto having an auto reply
+       * Will archive all but the last message, last one gets the replied status
+       * Sends an auto reply based on today's date and the category selected
+       */
+      function autoReply(template, contact, newestID){
+
+        //JQuery goes here
+        alert("Template: " + template + "\n" +
+              "Contact: " + contact + "\n" +
+              "Newest: " + newestID);
+
+      }
+
+      /** JS Sets reply to manual and opens up your email client
+       * Manual will time out after MANUAL_TIME_OUT and go back
+       * to default D status and back into your inbox */
+      function manualReply(replyType, contact, newestID){
+
+        //JQuery goes here
+        alert("Type: " + replyType + "\n" +
+              "Contact: " + contact + "\n" +
+              "Newest: " + newestID);
+
+      }
+
+      /** JS Sets this contacts assigned user and removes
+       * from this person's inbox */
+       function delegate(contact, delegate){
+
+         //JQuery goes here
+         alert("Contact: " + contact + "\n" +
+               "Delegate: " + delegate);
+
+       }
+    </script>
   </head>
   <body>
     <div class="contact">
@@ -94,6 +136,7 @@
             </td>
           </tr>
         <?php
+              $lastMessageID = $pendingMessages[$i]['id'];
             }
           }
         ?>
@@ -102,31 +145,37 @@
     <div id="spacer"></div>
     <div id="footer">
       <ul id="footer_menu"><!-- Menu Root -->
-        <li class="homeButton"><a href="#"></a></li>
+        <li class="homeButton"><a href="main.php"></a></li>
         <li><a href="#">Manual Reply</a>
           <ul class="dropup">
-            <li><a href="#">Email</a></li>
-            <li><a href="#">SMS</a></li>
-            <li><a href="#">Letter</a></li>
+            <li><a href="mailto:<?php echo $_GET['id']; ?>@crs.internal?subject=Type%20Your%20Subject%20Here&body=Type%20Your%20Message%20here" onclick="manualReply('E', '<?php echo $_GET['id']; ?>', '<?php echo $lastMessageID; ?>')">Email</a></li>
+            <li><a href="mailto:<?php echo $_GET['id']; ?>@crs.internal?subject=Subject%20Will%20Be%20Ignored&body=Type%20Your%20SMS%20here" onclick="manualReply('S', '<?php echo $_GET['id']; ?>', '<?php echo $lastMessageID; ?>')">SMS</a></li>
+            <li><a href="mailto:<?php echo $_GET['id']; ?>@crs.internal?subject=Subject%20Will%20Be%20Ignored&body=Type%20Your%20Letter%20here" onclick="manualReply('L', '<?php echo $_GET['id']; ?>', '<?php echo $lastMessageID; ?>')">Letter</a></li>
           </ul>
         </li>
         <li><a href="#">Auto Reply As...</a>
           <ul class="dropup">
-            <li><a href="#">General</a></li>
-            <li><a href="#">Bible</a></li>
-            <li><a href="#">Competition</a></li>
-            <li><a href="#">DVD</a></li>
+            <?php foreach($autoReply as $category){ ?>
+              <li>
+                <a href="javascript:void(0)" onclick="autoReply('<?php echo $category['id']; ?>', '<?php echo $_GET['id']; ?>', '<?php echo $lastMessageID; ?>')">
+                  <?php echo $category['label']; ?>
+                </a>
+              </li>
+            <?php } ?>
           </ul>
         </li>
         <li><a href="#">Delegate to...</a>
           <ul class="dropup">
-            <li><a href="#">Aziz Masih</a></li>
-            <li><a href="#">Preeti James</a></li>
-            <li><a href="#">Liza Gill</a></li>
-            <li><a href="#">Rachbinder Bunger</a></li>
+           <?php foreach($delegates as $user){ ?>
+              <li>
+                <a href="javascript:void(0)" onclick="delegate('<?php echo $_GET['id']; ?>', '<?php echo getLoggedInUserID(); ?>')">
+                  <?php echo $user['name']; ?>
+                </a>
+              </li>
+            <?php } ?>
           </ul>
         </li>
-        <li class="right"><a href="#">Logout</a></li>
+        <li class="right"><a href="logout.php">Logout</a></li>
       </ul>
     </div>
   </body>
