40b1567fb9b4d787f1bde08e96d6ed897a5c98b4
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-08-27 17:58:41 +0100

    Contact helper class

---
+++ src/com/thevoiceasia/contact/Contact.java
@@ -0,0 +1,8 @@
+package com.thevoiceasia.contact;
+
+public class Contact {
+
+	public String name = null, email = null, gender = null, phoneNumber = null, 
+			address = null, city = null, country = null;
+	
+}
