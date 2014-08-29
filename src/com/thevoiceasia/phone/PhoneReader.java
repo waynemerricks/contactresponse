fa32420b4c3d6418742fb8596e0fde12ac3ab28c
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-08-29 16:32:31 +0100

    Skeleton for PhoneReader class

--- src/com/thevoiceasia/phone/PhoneReader.java
+++ src/com/thevoiceasia/phone/PhoneReader.java
@@ -1,15 +1,242 @@
 package com.thevoiceasia.phone;
 
-public class PhoneReader {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import com.thevoiceasia.database.DatabaseHelper;
+import com.thevoiceasia.messages.MessageArchiver;
+
+public class PhoneReader extends MessageArchiver {
+
+	private DatabaseHelper phoneDatabase = null;
 	
+	public PhoneReader(String host, String user, String pass, String dbase,
+			String phoneHost, String phoneUser, String phonePass, String phoneDbase,
+			String archivePath){
+		
+		super(host, user, pass, dbase, archivePath);
+		
+		phoneDatabase = new DatabaseHelper(phoneHost, phoneDbase, phoneUser, 
+				phonePass);
+		
+	}
 	
 	/**
-	 * @param args
+	 * Connects to Phone database
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void connectToPhoneDB(){
+		
+		phoneDatabase.connect();
+		
+	}
+	
+	/**
+	 * Disconnects from Phone database
+	 */
+	public void disconnectFromPhoneDB(){
+		
+		phoneDatabase.disconnect();
+		
+	}
+	
+	/**
+	 * Entry point for PhoneReader
+	 * @param args 9 arguments expected:
+	 *   DB_HOST DB_USER DB_PASS DBASE PHONE_HOST PHONE_USER PHONE_PASS PHONE_DBASE MESSAGE_STORE_PATH
+	 */
+	public static void main(String[] args){
+		
+		if(args.length == 9){
+			
+			PhoneReader archiver = new PhoneReader(args[0], args[1], args[2], args[3], args[4], args[5],
+					args[6], args[7], args[8]);
+			
+			if(!archiver.isValidArchive()){
+				System.out.println("Message Store Path invalid, doesn't exist or has no write permissions: " + args[8]); //$NON-NLS-1$
+				System.exit(1);
+			}
+			
+			LOGGER.info("Phone Archiver: Started reading from  " + args[7]); //$NON-NLS-1$
+				
+			boolean go = true;
+			
+			while(go){
+				
+				try {
+					
+					archiver.connectToDB();
+					archiver.getMessages();
+					archiver.disconnectFromDB();
+					
+					LOGGER.finest("Phone Archiver: Sleeping for " + CHECK_PERIOD); //$NON-NLS-1$
+					sleep(1000 * CHECK_PERIOD);
+					
+				} catch (InterruptedException e) {
+					
+					LOGGER.info("Phone Archiver: Interrupted, exiting"); //$NON-NLS-1$
+					e.printStackTrace();
+					go = false;
+					
+				} finally {
+					
+					archiver.disconnectFromDB();
+					
+				}
+				
+			}
+			
+		}else{
+			
+			System.out.println("USAGE: DBHOST DBUSER DBPASS DBASE MAILHOST MAILUSER MAILPASS MAILDB MESSAGESTOREPATH"); //$NON-NLS-1$
+			System.exit(1);
+			
+		}
+		
+	}
 
+	/**
+	 * Get the last id we read from the phone table
+	 * @return 	-1 Error
+	 * 			-2 No results
+	 * 			else id
+	 */
+	private int getLastReadID(){
+		
+		int lastID = -1;
+		
+		String SQL = "SELECT `value` FROM `settings` WHERE `name` = 'lastPhoneID'"; //$NON-NLS-1$
+		
+		Statement selectID = null;
+		ResultSet results = null;
+		
+		try{
+			
+			selectID = database.getConnection().createStatement();
+			
+			if(selectID.execute(SQL)){
+				
+				results = selectID.getResultSet();
+			
+				lastID = -2;
+				
+				while(results.next())
+					lastID = results.getInt("value"); //$NON-NLS-1$
+				
+			}
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Phone Archiver: Error getting last phone ID"); //$NON-NLS-1$
+			e.printStackTrace();
+			
+		}finally{
+			
+			if(results != null){
+				
+				try{
+					results.close();
+				}catch(Exception e){}
+				
+				results = null;
+				
+			}
+			
+			if(selectID != null){
+				
+				try{
+					selectID.close();
+				}catch(Exception e){}
+				
+				selectID = null;
+				
+			}
+			
+		}
+		 
+		return lastID;
+		
+	}
+	
+	/**
+	 * Get Messages from the phone database
+	 */
+	public void getMessages() {
+		
+		int id = getLastReadID();
+		
+		phoneDatabase.connect();
+		
+		Statement selectPhones = null;
+		ResultSet results = null;
+		
+		try{
+		
+			String SQL = "SELECT * FROM `call_records` WHERE id > " + id + " ORDER BY id ASC";  //$NON-NLS-1$//$NON-NLS-2$
+			selectPhones = phoneDatabase.getConnection().createStatement();
+			
+			if(selectPhones.execute(SQL)){
+				
+				results = selectPhones.getResultSet();
+				
+				while(results.next())
+					processPhoneMessage(results);
+				
+			}
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Phone Archiver: Error while reading phone records " + id); //$NON-NLS-1$
+			e.printStackTrace();
+			
+		}finally{
+			
+			if(results != null){
+				
+				try{
+					results.close();
+				}catch(Exception e){}
+				
+				results = null;
+				
+			}
+			
+			if(selectPhones != null){
+				
+				try{
+					selectPhones.close();
+				}catch(Exception e){}
+				
+				selectPhones = null;
+				
+			}
+			
+		}
+		
+		phoneDatabase.disconnect();
+		
+	}
+
+	/**
+	 * Process the messages
+	 * @param results
+	 */
+	private void processPhoneMessage(ResultSet results) {
+		
+		int lastID = -1;
+		
+		try{
+		
+			lastID = results.getInt("id"); //$NON-NLS-1$
+			PhoneRecord pr = new PhoneRecord();
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Phone Archiver: Error getting record information" + lastID); //$NON-NLS-1$
+			e.printStackTrace();
+			
+		}
+		
 	}
 
 }
