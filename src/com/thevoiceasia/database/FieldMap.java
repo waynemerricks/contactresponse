a47d83b3d3fecb0e3360caf1e96d8144e1e94f02
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-29 22:42:01 +0100

    Added constructor to also keep hold of mapping and data associated
    Required for PhoneReader when you want to have a field with a list of
    approved options e.g. Language: English, Hindi, Other etc

--- src/com/thevoiceasia/database/FieldMap.java
+++ src/com/thevoiceasia/database/FieldMap.java
@@ -2,14 +2,42 @@ package com.thevoiceasia.database;
 
 public class FieldMap {
 
-	private String label = null, type = null;
+	private String label = null, type = null, map = null;
 	private int id = -1;
+	private String[] data = null;
 	
 	public FieldMap(int id, String label, String type){
 		
 		this.id = id;
 		this.label = label;
 		this.type = type;
+		
+	}
+	
+	public FieldMap(int id, String label, String type, String data, String map){
+		
+		this.id = id;
+		this.label = label;
+		this.type = type;
+		this.data = data.split(","); //$NON-NLS-1$
+		this.map = map;
+		
+	}
+	
+	public String getMap(){
+		
+		return map;
+		
+	}
+	
+	public String[] getData(){
+		
+		return data;
+	}
+	
+	public String getData(int index){
+		
+		return data[index];
 		
 	}
 	
