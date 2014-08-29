9dc298608ced6ecc64d7197e2cecd3b0f912affe
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-29 22:45:49 +0100

    Added HashMaps for db lookups was getting bored of typing results.get(X)
    Added: close(Statement, ResultSet), was also getting bored of closing
    statements and results with the try catch block and null setting
    
    Added: populateCustomIDs (not finished), gets custom fields with the 
    index of the field associated e.g. Language => English = 0
    
    Added: MAX_RECORDS limit to phone messages SQL

--- src/com/thevoiceasia/phone/PhoneReader.java
+++ src/com/thevoiceasia/phone/PhoneReader.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
+import com.thevoiceasia.contact.Contact;
 import com.thevoiceasia.database.DatabaseHelper;
 import com.thevoiceasia.messages.MessageArchiver;
 
@@ -14,7 +15,13 @@ public class PhoneReader extends MessageArchiver {
 
 	private DatabaseHelper phoneDatabase = null;
 	private HashMap<String, Integer> userIds = new HashMap<String, Integer>();
-	private HashMap<String, Integer> languageIds = new HashMap<String, Integer>();
+	private HashMap<String, HashMap<String, Integer>> customIds = 
+			new HashMap<String, HashMap<String,Integer>>();
+	private static final String[] CUSTOM_FIELDS = {"language", "religion",  //$NON-NLS-1$ //$NON-NLS-2$
+		"journeyStage", "topic"}; //$NON-NLS-1$ //$NON-NLS-2$
+	private static final String[] STRING_FIELDS = {"caller_name", "number",   //$NON-NLS-1$//$NON-NLS-2$
+		"conversation", "location", "topic"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private static final int MAX_RECORDS = 10;
 	
 	public PhoneReader(String host, String user, String pass, String dbase,
 			String phoneHost, String phoneUser, String phonePass, String phoneDbase,
@@ -26,9 +33,49 @@ public class PhoneReader extends MessageArchiver {
 				phonePass);
 		
 		populateUserIDs();
+		populateCustomIDs();
 		
 	}
-	
+
+	/**
+	 * Grabs all the custom fields from the contact_fields table
+	 */
+	private void populateCustomIDs() {
+
+		String SQL = "SELECT * FROM `contact_fields`"; //$NON-NLS-1$
+		
+		Statement selectCustom = null;
+		ResultSet results = null;
+		
+		try{
+			
+			selectCustom = database.getConnection().createStatement();
+			
+			if(selectCustom.execute(SQL)){
+				
+				results = selectCustom.getResultSet();
+			
+				while(results.next()){
+				
+					//TODO
+				
+				}
+				
+			}
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Phone Archiver: Error getting custom contact fields"); //$NON-NLS-1$
+			e.printStackTrace();
+			
+		}finally{
+			
+			close(selectCustom, results);
+			
+		}
+		
+	}
+
 	private void populateUserIDs() {
 		// TODO Auto-generated method stub
 		
@@ -144,25 +191,7 @@ public class PhoneReader extends MessageArchiver {
 			
 		}finally{
 			
-			if(results != null){
-				
-				try{
-					results.close();
-				}catch(Exception e){}
-				
-				results = null;
-				
-			}
-			
-			if(selectID != null){
-				
-				try{
-					selectID.close();
-				}catch(Exception e){}
-				
-				selectID = null;
-				
-			}
+			close(selectID, results);
 			
 		}
 		 
@@ -170,6 +199,34 @@ public class PhoneReader extends MessageArchiver {
 		
 	}
 	
+	/**
+	 * Helper method to close statements and resultsets
+	 * @param statement
+	 * @param results
+	 */
+	private void close(Statement statement, ResultSet results){
+		
+		if(results != null){
+			
+			try{
+				results.close();
+			}catch(Exception e){}
+			
+			results = null;
+			
+		}
+
+		if(statement != null){
+			
+			try{
+				statement.close();
+			}catch(Exception e){}
+			
+			statement = null;
+			
+		}
+		
+	}
 	/**
 	 * Get Messages from the phone database
 	 */
@@ -184,7 +241,8 @@ public class PhoneReader extends MessageArchiver {
 		
 		try{
 		
-			String SQL = "SELECT * FROM `call_records` WHERE id > " + id + " ORDER BY id ASC";  //$NON-NLS-1$//$NON-NLS-2$
+			String SQL = "SELECT * FROM `call_records` WHERE id > " + id + //$NON-NLS-1$
+					" ORDER BY id ASC LIMIT " + MAX_RECORDS;  //$NON-NLS-1$
 			selectPhones = phoneDatabase.getConnection().createStatement();
 			
 			if(selectPhones.execute(SQL)){
@@ -203,25 +261,7 @@ public class PhoneReader extends MessageArchiver {
 			
 		}finally{
 			
-			if(results != null){
-				
-				try{
-					results.close();
-				}catch(Exception e){}
-				
-				results = null;
-				
-			}
-			
-			if(selectPhones != null){
-				
-				try{
-					selectPhones.close();
-				}catch(Exception e){}
-				
-				selectPhones = null;
-				
-			}
+			close(selectPhones, results);
 			
 		}
 		
@@ -257,7 +297,6 @@ public class PhoneReader extends MessageArchiver {
 			
 			lastID = results.getInt("id"); //$NON-NLS-1$
 			
-			pr.name = checkNull(results.getString("caller_name")); //$NON-NLS-1$
 			pr.setAddress(results.getString("address"),  //$NON-NLS-1$
 					results.getString("postcode")); //$NON-NLS-1$
 			
@@ -297,12 +336,34 @@ public class PhoneReader extends MessageArchiver {
 			else
 				pr.answeredBy = -1;
 			
-			key = checkNull(results.getString("language").toLowerCase().trim()); //$NON-NLS-1$
 			
-			if(languageIds.containsKey(key))
-				pr.language = languageIds.get(key);
+			for(int i = 0; i < STRING_FIELDS.length; i++){
+				
+				String value = checkNull(results.getString(STRING_FIELDS[i])
+						.trim());
+				
+				pr.setString(STRING_FIELDS[i], value);
+				
+			}
 			
-			//TODO other fields
+			for(int i = 0; i < CUSTOM_FIELDS.length; i++){
+				
+				key = checkNull(results.getString(CUSTOM_FIELDS[i])
+						.toLowerCase().trim());
+				
+				if(customIds.containsKey(CUSTOM_FIELDS[i]) &&
+						customIds.get(CUSTOM_FIELDS[i]).containsKey(key)){
+					
+					pr.setCustomField(CUSTOM_FIELDS[i], 
+							customIds.get(CUSTOM_FIELDS[i]).get(key));
+					
+				}
+				
+			}
+			
+			new Contact(pr);
+			
+			
 		}catch(SQLException e){
 			
 			LOGGER.severe("Phone Archiver: Error getting record information" + lastID); //$NON-NLS-1$
@@ -311,5 +372,5 @@ public class PhoneReader extends MessageArchiver {
 		}
 		
 	}
-
+	
 }
