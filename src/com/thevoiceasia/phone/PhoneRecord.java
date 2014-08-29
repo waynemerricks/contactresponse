ca7dc3940f4b076b817b73d80d3e1aff16e93c8a
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-29 22:43:47 +0100

    Swapped to HashMaps for fields instead of lots of hard strings
    Added: setCustomField(key, value)
    Added: getCustomField(key)
    Added: getPhoneNumber
    Added: setString(key, value) replaces statics like name, number etc

--- src/com/thevoiceasia/phone/PhoneRecord.java
+++ src/com/thevoiceasia/phone/PhoneRecord.java
@@ -1,13 +1,17 @@
 package com.thevoiceasia.phone;
 
+import java.util.HashMap;
+
 public class PhoneRecord {
 	
-	public String name = null, email = null,  gender = null, 
-			religion = null, journeyStage = null, topic = null,
-			number = null, conversation = null, location = null;
+	public String email = null,  gender = null, 
+			conversation = null, location = null;
 	private String address = null;
-	public int answeredBy = -1, language = -1;
+	public int answeredBy = -1;
 	public long time = -1l;
+	
+	private HashMap<String, String> stringFields = new HashMap<String, String>();
+	private HashMap<String, Integer> customIDs = new HashMap<String, Integer>();
 	
 	/**
 	 * Sets the address of this object by combining address + postcode
@@ -45,6 +49,66 @@ public class PhoneRecord {
 			check = null;
 		
 		return check;
+		
+	}
+
+	/**
+	 * Set custom field in the hash map
+	 * @param key
+	 * @param value
+	 */
+	public void setCustomField(String key, Integer value) {
+
+		customIDs.put(key, value);
+		
+	}
+	
+	/**
+	 * Get a custom field from the hash map
+	 * @param key
+	 * @return
+	 */
+	public int getCustomField(String key){
+		
+		return customIDs.get(key);
+		
+	}
+	
+	/**
+	 * Returns the phone number for this object
+	 * @return
+	 */
+	public String getPhoneNumber(){
+		
+		return stringFields.get("number"); //$NON-NLS-1$
+		
+	}
+
+	/**
+	 * Sets a straight forward string value in a hash map
+	 * Saves having lots of private vars in advance
+	 * @param key
+	 * @param value
+	 */
+	public void setString(String key, String value) {
+		
+		/* If its a phone number strip the leading 0s for international
+		 * Otherwise it buggers up text messaging
+		 * 
+		 * Also add in 44 for numbers that are 11 long and start with 1 zero.
+		 */
+		if(key.equals("number")){ //$NON-NLS-1$
+			
+			if(value.length() > 11 && value.startsWith("0")) //$NON-NLS-1$
+				while(value.startsWith("0")) //$NON-NLS-1$
+					value = value.substring(1);
+			else if(value.length() == 11 && value.startsWith("0") && //$NON-NLS-1$
+				!value.substring(1).startsWith("0")) //$NON-NLS-1$
+				value = "44" + value.substring(1); //$NON-NLS-1$
+			
+		}
+		
+		stringFields.put(key, value);
 		
 	}
 	
