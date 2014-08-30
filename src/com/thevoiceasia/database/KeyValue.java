f9f2d978a463c848fa73913aeb067ce6080c53f0
Wayne Merricks <merricks.wayne@googlemail.com>
2014-08-30 12:45:11 +0100

    Helper class to store key => value mappings for use in an array list
    HashMap doesn't guarantee Iterator order, in theory order should be the
    same as long as Map doesn't change but read values into an ArrayList
    to be safe.

---
+++ src/com/thevoiceasia/database/KeyValue.java
@@ -0,0 +1,14 @@
+package com.thevoiceasia.database;
+
+public class KeyValue {
+
+	public String key = null, value = null;
+	
+	public KeyValue(String key, String value){
+		
+		this.key = key;
+		this.value = value;
+		
+	}
+	
+}
