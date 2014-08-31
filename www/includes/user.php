5167848d7fd203a6c75451e4f205a9beec5ebb6f
Wayne Merricks <merricks.wayne@googlemail.com>
2014-09-01 00:13:37 +0100

    Added group based delegate rules... needs testing

--- www/includes/user.php
+++ www/includes/user.php
@@ -81,7 +81,31 @@
     $delegate = FALSE;
 
     if(hasPermission('CAN_DELEGATE_UNDERLING') ||
-        hasPermission('CAN_DELEGATE_ALL'))
+        hasPermission('CAN_DELEGATE_ALL') ||
+        hasPermission('CAN_DELEGATE_GROUP'))
+      $delegate = TRUE;
+
+    return $delegate;
+
+  }
+
+
+  function canDelegateGroup(){
+
+    $delegate = FALSE;
+
+    if(hasPermission('CAN_DELEGATE_ALL'))
+      $delegate = TRUE;
+
+    return $delegate;
+
+  }
+
+  function canDelegateUnderlings(){
+
+    $delegate = FALSE;
+
+    if(hasPermission('CAN_DELEGATE_UNDERLING'))
       $delegate = TRUE;
 
     return $delegate;
@@ -143,6 +167,150 @@
 
   }
 
+
+  /**
+   * Gets the highest group we're a member of for delegation purposes
+   */
+  function getHighestGroups($mysqli, $userGroups){
+
+    $member = array();
+    $sql = 'SELECT `role_id` FROM `role_members` WHERE `user_id` = ' . 
+              getLoggedInUserID();
+    $result = $mysqli->query($sql);
+
+    while($row = $result->fetch_assoc())
+      $member[] = $row['role_id'];
+
+    $result->close();
+    $rootGroup = NULL;
+
+    foreach($userGroups as $id => $parent){
+
+      if(in_array($id, $member)){
+
+        $inParent = TRUE;
+
+        while($inParent == TRUE){
+
+          if(in_array($parent, $member))
+            $parent = $userGroups[$parent];
+          else{
+
+            $rootGroup = $parent;
+            $inParent = FALSE;
+
+          }
+
+        }
+
+      }
+
+    }
+
+    $lastKey = NULL;
+
+    foreach($rootGroup as $key => $value)
+      $lastKey = $key;
+
+    return $rootGroup[$lastKey];
+
+  }
+
+  function getUserGroups($mysqli){
+
+    $sql = 'SELECT `id`, `parent` FROM `roles` WHERE `active_permissions` = \'\'';
+
+    $result = $mysqli->query($sql);
+    $groups = array();
+
+    while($row = $result->fetch_assoc()){
+
+      $group = array();
+      $group[$row['id']] = $row['parent'];
+      $groups[] = $group;
+
+    }
+
+    $result->close();
+
+    return $groups;
+
+  }
+
+  function getGroupMembers($group, $mysqli){
+
+    $members = array();
+
+    $sql = 'SELECT `user_id` FROM `role_members` WHERE `role_id` = ' . $group;
+
+    $result = $mysqli->query($sql);
+
+    while($row = $result->fetch_assoc())
+      $members[] = $row['user_id'];
+
+    $result->close();
+
+    return $members;
+
+  }
+
+  function getGroupMembersFromRoot($root, $groups, $mysqli){
+
+    //$groups[id] => parent
+    //$root = root group e.g. 6
+    $members = array();
+    $roles = array();
+    $i = -1;
+
+    while($root != NULL){
+
+      foreach($groups as $array){
+
+        foreach($array as $id => $parent){
+
+          if($parent == $root)
+            $roles[] = $id;
+
+        }
+
+      }
+      //Here we'll have all the roles that had this parent
+      //move root to next $role
+
+      $i++;
+
+      if(isset($roles[$i]))
+        $root = $roles[$i];
+      else
+        $root = NULL;
+
+    }
+
+    //Here we'll have all the roles that have members we need
+    $sql = 'SELECT `user_id` FROM `role_members` WHERE ';
+
+    if(sizeof($roles) > 0){
+
+      $sql .= '`role_id` = ' . $roles[0];
+
+      for($i = 1; $i < sizeof($roles); $i++)
+        $sql .= ' OR `role_id` = ' . $roles[$i];
+
+      $sql .= ' GROUP BY `user_id`';
+
+      $result = $mysqli->query($sql);
+
+      while($row = $result->fetch_assoc())
+        $members[] = $row['user_id'];
+
+      $result->close();
+
+    }
+
+    return $members;
+
+  }
+
   /**
    * Returns a list of users we can delegate to
    */
@@ -150,19 +318,56 @@
 
     $delegates = array();
 
-    //TODO Restrict based on user thats logged in
-    $sql = 'SELECT `id`, `name` FROM `users` ORDER BY `name` ASC';
-    $result = $mysqli->query($sql);
+    if(canDelegate()){
 
-    while($row = $result->fetch_assoc()){
+      $userGroups = getUserGroups($mysqli);
+      $rootGroup = getHighestGroups($mysqli, $userGroups);
 
-      $delegate = array();
+      if(canDelegateGroup() && !canDelegateUnderlings())
+        $members = getGroupMembers($rootGroup, $mysqli);
+      else if(!canDelegateGroup() && canDelegateUnderlings())
+        $members = getGroupMembersFromRoot($rootGroup, $userGroups, $mysqli);
+      else if(canDelegateGroup() && canDelegateUnderlings()){
 
-      if($row['id'] != getLoggedInUserID()){
+        $rootDelegates = getGroupMembers($rootGroup, $mysqli);
+        $childDelegates = getGroupMembersFromRoot($rootGroup, $userGroups, $mysqli);
 
-        $delegate['id'] = $row['id'];
-        $delegate['name'] = $row['name'];
-        $delegates[] = $delegate;
+        $members = array_unique(array_merge($rootDelegates, $childDelegates));
+
+      }
+
+      if(sizeof($members) > 0){
+
+        $first = TRUE;
+        $ids = '';
+
+        foreach($members as $user){
+
+          if($first === TRUE){
+            $ids .= $user;
+            $first = FALSE;
+          }else
+            $ids .= ',' . $user;
+        }
+
+        $sql = 'SELECT `id`, `name` FROM `users` WHERE `id` IN (' . $ids .
+               ') ORDER BY `name` ASC';
+
+        $result = $mysqli->query($sql);
+
+        while($row = $result->fetch_assoc()){
+
+          $delegate = array();
+
+          if($row['id'] != getLoggedInUserID()){
+
+            $delegate['id'] = $row['id'];
+            $delegate['name'] = $row['name'];
+            $delegates[] = $delegate;
+
+          }
+
+        }
 
       }
 
