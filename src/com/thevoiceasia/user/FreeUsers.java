210a9be4bb50570a04fd93df0e10613c90ee69bb
Wayne Merricks <waynemerricks@thevoiceasia.com>
2014-09-05 17:30:41 +0100

    Helper classes that store user contact counts and calculate next user

---
+++ src/com/thevoiceasia/user/FreeUsers.java
@@ -0,0 +1,150 @@
+package com.thevoiceasia.user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.thevoiceasia.database.DatabaseHelper;
+
+public class FreeUsers {
+	
+	private DatabaseHelper database = null;
+	private ArrayList<User> users = null;
+	private User nextUser = null;
+	private int currentIndex = 0;
+	
+	private static final Logger LOGGER = Logger.getLogger(User.class.getName());//Logger
+	
+	/**
+	 * Object that manages users for contact assigning purposes
+	 * @param database
+	 */
+	public FreeUsers(DatabaseHelper database){
+		
+		this.database = database;
+		populateUsers();
+		
+	}
+	
+	/**
+	 * Add a contact to the specified user (just increments the current count)
+	 * @param id
+	 */
+	public void addContact(int id){
+		
+		boolean found = false;
+		int i = 0;
+		
+		while(!found && i < users.size()){
+			
+			if(users.get(i).getId() == id){
+				
+				users.get(i).addCurrent(1, database.getConnection());
+				found = true;
+				
+			}
+			
+			i++;
+			
+		}
+		
+	}
+	
+	/**
+	 * Get the next user who can get new contacts (e.g. the ones who haven't 
+	 * reached max contacts yet)
+	 * @return User or null if there are no more users who can get new contacts
+	 */
+	public int getNextAvailableUser(){
+		
+		int id = -1;;
+		User user = null;
+		
+		if(nextUser != null){
+			
+			user = nextUser;
+			currentIndex++;//increment index to next in users array
+			
+			if(currentIndex >= users.size())//if we've gone over the array size start again from 0
+				currentIndex = 0;
+			
+			while(!users.get(currentIndex).canHaveMoreContacts() && 
+					currentIndex < users.size())//while we can't have more contacts increment index
+				currentIndex++;
+			
+			if(currentIndex < users.size())
+				nextUser = users.get(currentIndex);
+			else
+				nextUser = null;
+			
+		}
+		
+		if(user != null)
+			id = user.getId();
+		
+		return id;
+		
+	}
+	
+	/**
+	 * Read in the users from the db
+	 */
+	private void populateUsers(){
+		
+		Statement selectUsers = null;
+		ResultSet results = null;
+		
+		String SQL = "SELECT `id`, `max_contacts`, `current_contacts` " + //$NON-NLS-1$
+				"FROM `users` WHERE `active` = 'Y' AND " + //$NON-NLS-1$
+				"`current_contacts` < `max_contacts`"; //$NON-NLS-1$
+		
+		try{
+			
+			selectUsers = database.getConnection().createStatement();
+			
+			if(selectUsers.execute(SQL)){
+				
+				results = selectUsers.getResultSet();
+				
+				while(results.next())
+					users.add(new User(results.getInt("id"), //$NON-NLS-1$
+							results.getInt("max_contacts"), //$NON-NLS-1$
+							results.getInt("current_contacts"))); //$NON-NLS-1$
+					
+				if(users.size() > 0)
+					nextUser = users.get(0);
+				
+			}
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Error getting user list"); //$NON-NLS-1$
+			e.printStackTrace();
+			
+		}finally{
+			
+			if(selectUsers != null){
+				
+				try{
+					selectUsers.close();
+				}catch(Exception e){}
+				selectUsers = null;
+				
+			}
+			
+			if(results != null){
+				
+				try{
+					results.close();
+				}catch(Exception e){}
+				results = null;
+				
+			}
+			
+		}
+		
+	}
+	
+}
---
+++ src/com/thevoiceasia/user/User.java
@@ -0,0 +1,121 @@
+package com.thevoiceasia.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class User {
+
+	private int userId = -1, maxContacts = -1, currentContacts = -1;
+	private static final Logger LOGGER = Logger.getLogger(User.class.getName());//Logger
+	
+	/**
+	 * Creates a user record for keeping tally of contacts count
+	 * @param id user id
+	 * @param max max number of contacts
+	 * @param current current number of contacts
+	 */
+	public User(int id, int max, int current){
+		
+		this.userId = id;
+		this.maxContacts = max;
+		this.currentContacts = current;
+		
+	}
+	
+	/**
+	 * true if we have reached max number of contacts
+	 * @return
+	 */
+	public boolean canHaveMoreContacts(){
+		
+		boolean more = true;
+		
+		if(maxContacts < currentContacts)
+			more = false;
+		
+		return more;
+		
+	}
+	
+	/**
+	 * Returns the user id
+	 * @return
+	 */
+	public int getId(){
+		
+		return userId;
+		
+	}
+	
+	/**
+	 * Returns max contacts
+	 * @return
+	 */
+	public int getMaxContacts(){
+		
+		return maxContacts;
+		
+	}
+	
+	/**
+	 * returns current contacts
+	 * @return
+	 */
+	public int getCurrentContacts(){
+		
+		return currentContacts;
+		
+	}
+	
+	/**
+	 * Adds the value to the current contacts field of this user
+	 * @param value +/- value e.g. current_contacts = current_contacts + value
+	 * @param mysql MySQL Connection to use (write needed)
+	 * @return true if no errors
+	 */
+	public boolean addCurrent(int value, Connection mysql){
+		
+		boolean success = false;
+		currentContacts += value;
+		
+		String SQL = "UPDATE `users` SET " + //$NON-NLS-1$
+				"`current_contacts` = `current_contacts` + ? WHERE `id` = ?"; //$NON-NLS-1$
+		
+		PreparedStatement update = null;
+		
+		try{
+			
+			update = mysql.prepareStatement(SQL);
+			update.setInt(1, value);
+			update.setInt(2, userId);
+			
+			int rows = update.executeUpdate();
+			
+			if(rows > 0)
+				success = true;
+			
+		}catch(SQLException e){
+			
+			LOGGER.severe("Error while updating current contacts for user " +  //$NON-NLS-1$
+					userId);
+			e.printStackTrace();
+			
+		}finally{
+			
+			if(update != null){
+				
+				try{
+					update.close();
+				}catch(Exception e){}
+				update = null;
+				
+			}
+		}
+		
+		return success;
+		
+	}
+	
+}
