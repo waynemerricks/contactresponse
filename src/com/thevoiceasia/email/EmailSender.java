33b54d70a6b9565560ad26677d82566a80b3be9c
Wayne Merricks <merricks.wayne@googlemail.com>
2014-09-07 16:19:48 +0100

    Added class responsible for sending out emails

---
+++ src/com/thevoiceasia/email/EmailSender.java
@@ -0,0 +1,13 @@
+package com.thevoiceasia.email;
+
+public class EmailSender {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
