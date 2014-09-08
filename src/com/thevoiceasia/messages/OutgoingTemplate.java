5429630e2516a2c5dd4b33e9667df4db733ea336
Wayne Merricks <merricks.wayne@googlemail.com>
2014-09-08 22:47:45 +0100

    Fully Implemented OutgoingTemplate object with language support

--- src/com/thevoiceasia/messages/OutgoingTemplate.java
+++ src/com/thevoiceasia/messages/OutgoingTemplate.java
@@ -1,16 +1,32 @@
 package com.thevoiceasia.messages;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.thevoiceasia.database.KeyValue;
 
 public class OutgoingTemplate {
 	
 	private int id = -1, priority = -1;
-	private String fromEmail = null;
+	private String from = null;
+	private HashMap<Integer, TemplateContainer> languageContent = 
+			new HashMap<Integer, TemplateContainer>();//Language (lower case), Container
 	private boolean exclusive = true;
 	private Date date = null;
 	private SimpleDateFormat mysqlTimeStamp = 
 			new SimpleDateFormat("yyyyMMddHHmmss"); //$NON-NLS-1$
+	
+	protected static final Logger LOGGER = Logger.getLogger("com.thevoiceasia.messages.OutgoingTemplate"); //$NON-NLS-1$
+	protected static final Level LEVEL = Level.INFO;//Logging level of this class
+	
 	
 	/**
 	 * Outgoing template helper object
@@ -20,11 +36,14 @@ public class OutgoingTemplate {
 	 * @param priority
 	 * @param timestamp
 	 */
-	public OutgoingTemplate(int id, String fromEmail, boolean exclusive, 
-			int priority, String timestamp){
+	public OutgoingTemplate(int id, String from, boolean exclusive, 
+			int priority, String timestamp, String templateBasePath, 
+			KeyValue[] languages){
+		
+		LOGGER.setLevel(LEVEL);
 		
 		this.id = id;
-		this.fromEmail = fromEmail;
+		this.from = from;
 		this.exclusive = exclusive;
 		this.priority = priority;
 		
@@ -37,6 +56,155 @@ public class OutgoingTemplate {
 			}
 			
 		}
+		
+		String base = templateBasePath + System.getProperty("file.separator") +  //$NON-NLS-1$
+				id;
+		
+		for(int i = 0; i < languages.length; i++){
+			
+			TemplateContainer tc = new TemplateContainer();
+			
+			//EMAIL
+			String[] temp = readTemplateFile(base + "_email_" + languages[i].value); //$NON-NLS-1$
+			tc.subject = temp[0];
+			tc.email = temp[1];
+			
+			//SMS
+			temp = readTemplateFile(base + "_sms_" + languages[i].value); //$NON-NLS-1$
+			tc.sms = temp[0];
+			
+			//LETTER
+			temp = readTemplateFile(base + "_letter_" + languages[i].value); //$NON-NLS-1$
+			tc.letter = temp[0];
+			
+			//Add to map
+			languageContent.put(Integer.parseInt(languages[i].key), tc);
+			
+		}
+		
+	}
+	
+	/**
+	 * Gets the SMS template for the given language
+	 * @param language use language id in languages table
+	 * @return
+	 */
+	public String getSMS(int language){
+		
+		return languageContent.get(language).sms;
+		
+	}
+	
+	/**
+	 * Gets the Email Subject template for the given language
+	 * @param language use language id in languages table
+	 * @return
+	 */
+	public String getSubject(int language){
+		
+		return languageContent.get(language).subject;
+		
+	}
+	
+	/**
+	 * Gets the Email Body template for the given language
+	 * @param language use language id in languages table
+	 * @return
+	 */
+	public String getEmailBody(int language){
+		
+		return languageContent.get(language).email;
+		
+	}
+	
+	/**
+	 * Gets the Letter template for the given language
+	 * @param language use language id in languages table
+	 * @return
+	 */
+	public String getLetter(int language){
+		
+		return languageContent.get(language).letter;
+		
+	}
+	
+	/**
+	 * Reads the given file and returns the content
+	 * @param path path to read
+	 * @return Array[0] = content or in emails case 0 = subject, 1 = content
+	 */
+	private String[] readTemplateFile(String path){
+		
+		int strings = 1;
+		
+		if(path.contains("email")) //$NON-NLS-1$
+			strings = 2;
+		
+		String[] returnValues = new String[strings];
+		
+		for(int i = 0; i < strings; i++)
+			returnValues[i] = ""; //$NON-NLS-1$
+		
+		File template = new File(path);
+		BufferedReader reader = null;
+		
+		if(template.exists() && template.canRead()){
+			
+			try{
+				
+				reader = new BufferedReader(
+						new FileReader(template));
+				
+				boolean done = false;
+				boolean first = true;
+				
+				while(!done){
+					
+					String line = reader.readLine();
+					
+					if(line == null)
+						done = true;
+					else{
+						
+						if(first && strings == 2 && line.startsWith("S:")){ //$NON-NLS-1$
+							
+							first = false;
+							returnValues[0] = line.substring(2);//Subject
+							
+						}else
+							returnValues[1] += line + "\n"; //$NON-NLS-1$
+							
+					}
+					
+				}
+				
+			}catch(FileNotFoundException e){
+				
+				LOGGER.severe("Template file not found " + template.getName()); //$NON-NLS-1$
+				e.printStackTrace();
+				
+			}catch(IOException e){
+				
+				LOGGER.severe("Template IO Error " + template.getName()); //$NON-NLS-1$
+				e.printStackTrace();
+				
+			}finally{
+				
+				if(reader != null){
+					
+					try{
+						reader.close();
+					}catch(Exception e){}
+					
+					reader = null;
+					
+				}
+				
+			}
+			
+		}
+		
+		return returnValues;
 		
 	}
 	
@@ -67,21 +235,33 @@ public class OutgoingTemplate {
 		
 	}
 	
+	/**
+	 * Return the id of this object
+	 * @return
+	 */
 	public int getId(){
 		
 		return id;
 		
 	}
 	
+	/**
+	 * Return the priority
+	 * @return
+	 */
 	public int getPriority(){
 		
 		return priority;
 		
 	}
 	
-	public String getFromEmail(){
+	/**
+	 * Return the address this email/sms should be from
+	 * @return
+	 */
+	public String getFrom(){
 		
-		return fromEmail;
+		return from;
 		
 	}
 	
---
+++ src/com/thevoiceasia/messages/TemplateContainer.java
@@ -0,0 +1,7 @@
+package com.thevoiceasia.messages;
+
+public class TemplateContainer {
+
+	public String email = null, subject = null, sms = null, letter = null; 
+	
+}
